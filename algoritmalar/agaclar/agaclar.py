4988d071b40a73188fa426740e4dc139fb00c452
sezer-muhammed <e230531@metu.edu.tr>
2022-12-29 20:59:45 +0300

    trees

---
+++ algoritmalar/agaclar/agaclar.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+from sklearn.tree import export_text
+from matplotlib import pyplot as plt
+
+iris = load_iris()
+X, y = iris.data, iris.target
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, y)
+
+tree.plot_tree(clf)
+plt.show()
+
+import graphviz 
+dot_data = tree.export_graphviz(clf, out_file=None) 
+graph = graphviz.Source(dot_data) 
+graph.render("algoritmalar/agaclar/cicekler")
+dot_data = tree.export_graphviz(clf, out_file=None, 
+                     feature_names=iris.feature_names,  
+                     class_names=iris.target_names,  
+                     filled=True, rounded=True,  
+                     special_characters=True)  
